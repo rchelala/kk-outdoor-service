e77437e8d0b68b68f9df52298d3907d2380dd4ad
Robert Chelala <Robertchelala@gmail.com>
2026-04-26 02:52:53 -0700

    feat: wire up React Router with layout shell (Navbar + Footer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { Home } from './pages/Home'
+import { Services } from './pages/Services'
+import { Book } from './pages/Book'
+import { Contact } from './pages/Contact'
+
 export default function App() {
-  return <div className="animated-gradient min-h-screen" />
+  return (
+    <BrowserRouter>
+      <div className="animated-gradient min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/book" element={<Book />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
 }
